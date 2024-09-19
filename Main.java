c091ee2bbe0c4ffbfeb414f0e0243e56d48242ef
nonato-barroso91 <nonatobarroso14@gmail.com>
2024-09-19 20:27:32 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,20 @@
+import exemplo.AreaCirculo.AreaCirculo;
+import exemplo.CelsiusParaFahrenheit.CelsiusParaFahrenheit;
+import exemplo.HelloWorld.HelloWorld;
+import exemplo.MaiorDeTresNumeros.MaiorDeTresNumeros;
+import exemplo.MediaTresNumeros.MediaTresNumeros;
+import exemplo.ParOuImpar.ParOuImpar;
+import exemplo.SomaDoisNumeros.SomaDoisNumeros;
+import exemplo.MetrosParaCentimetros.MetrosParaCentimetros;
+public class Main {
+    public static void main(String[] args) {
+        AreaCirculo.main(args);
+        CelsiusParaFahrenheit.main(args);
+        HelloWorld.main(args);
+        MaiorDeTresNumeros.main(args);
+        MediaTresNumeros.main(args);
+        ParOuImpar.main(args);
+        SomaDoisNumeros.main(args);
+        MetrosParaCentimetros.main(args);
+    }
+}
